a4b6c290b618f94a2b162c5bc327f0fcc6a58d43
yahia <y_kilany@aucegypt.edu>
2023-05-05 20:57:42 +0300

    modified corpus.cpp

--- Corpus.cpp
+++ Corpus.cpp
@@ -4,14 +4,16 @@ CS2 project: 2- Simple palagarism detection utility using string matching
 
 #include "Corpus.h"
 
-/**
- * Corpus implementation
- */
-
-
 void Corpus::addDocument(Document Doc) {
+    Documents.push_back(Doc);
     return;
 }
 vector<Document> Corpus::getDocuments() const{
     return Documents;
+}
+Document Corpus::getDocument(int index) const
+{
+    if(index>Documents.size())
+        exit(1);
+    return Documents[index];
 }
