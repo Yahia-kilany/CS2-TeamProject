2537c96a4c02c161fb0b7ae6dbce9243d981ae6c
yahia <y_kilany@aucegypt.edu>
2023-05-05 22:03:22 +0300

    updated the main.cpp file

--- main.cpp
+++ main.cpp
@@ -8,41 +8,91 @@
 #include <RabinKarpMatcher.h>
 #include <kmpMatcher.h>
 #include <BooyerMooreMatcher.h>
-int main() {
-    string text = "This is a sample text that contains some text.";
-    string pattern = "text";
-    
+using namespace std;
+int main () {
+
     BruteForceMatcher bruteForceMatcher;
     RabinKarpMatcher rabinKarpMatcher;
-    
-    vector<int> bfMatches = bruteForceMatcher.match(text, pattern);
-    vector<int> rkMatches = rabinKarpMatcher.match(text, pattern);
-    
+    kmpMatcher kmpMatcher;
+    BooyerMooreMatcher booyerMooreMatcher;
+
+    vector<string> bfMatches = bruteForceMatcher.match (testdoc , corpus);
+    vector<string> rkMatches = rabinKarpMatcher.match (testdoc , corpus);
+    vector<string> kmpMatcher = kmpMatcher.match (testdoc , corpus);
+    vector<string> bmMatches = booyerMooreMatcher.match (testdoc , corpus);
     // Compare the results of the two algorithms
-    if (bfMatches.size() == rkMatches.size()) {
-        cout << "Both algorithms found the same number of matches." << endl;
-    } else {
-        cout << "The algorithms found a different number of matches." << endl;
+    std::cout << "The titles of the documents that were plagarized (As detected by brute force):" << std::endl;
+    for (vector<string>::iterator itr = bfMatches.begin ();itr != bfMatches.end ();itr++)
+    {
+        std::cout << *itr << std::endl;
     }
-    
+    std::cout << "The titles of the documents that were plagarized (As detected by brute force):" << std::endl;
+    for (vector<string>::iterator itr = rkMatches.begin ();itr != bfMatches.end ();itr++)
+    {
+        std::cout << *itr << std::endl;
+    }
+    std::cout << "The titles of the documents that were plagarized (As detected by brute force):" << std::endl;
+    for (vector<string>::iterator itr = kmpMatches.begin ();itr != bfMatches.end ();itr++)
+    {
+        std::cout << *itr << std::endl;
+    }
+    std::cout << "The titles of the documents that were plagarized (As detected by brute force):" << std::endl;
+    for (vector<string>::iterator itr = bmMatches.begin ();itr != bfMatches.end ();itr++)
+    {
+        std::cout << *itr << std::endl;
+    }
     // Compare the time it takes to run each algorithm
-    auto start = chrono::high_resolution_clock::now();
-    bruteForceMatcher.match(text, pattern);
-    auto end = chrono::high_resolution_clock::now();
-    auto bfDuration = chrono::duration_cast<chrono::microseconds>(end - start).count();
-    
-    start = chrono::high_resolution_clock::now();
-    rabinKarpMatcher.match(text, pattern);
-    end = chrono::high_resolution_clock::now();
-    auto rkDuration = chrono::duration_cast<chrono::microseconds>(end - start).count();
-    
-    if (bfDuration < rkDuration) {
-        cout << "Brute force algorithm is faster." << endl;
-    } else if (bfDuration > rkDuration) {
-        cout << "Rabin-Karp algorithm is faster." << endl;
-    } else {
-        cout << "Both algorithms took the same amount of time to run." << endl;
+    auto start = chrono::high_resolution_clock::now ();
+    bruteForceMatcher.match (testdoc , corpus);
+    auto end = chrono::high_resolution_clock::now ();
+    auto bfDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
+    cout<<"The brute Force took :"<<bfDuration<<" ms"<<" to excute"<<endl;
+    start = chrono::high_resolution_clock::now ();
+    rabinKarpMatcher.match (testdoc , corpus);
+    end = chrono::high_resolution_clock::now ();
+    auto rkDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
+    cout<<"The rabin karp took :"<<rkDuration<<" ms"<<" to excute"<<endl;
+    start = chrono::high_resolution_clock::now ();
+    kmpMatcher.match (testdoc , corpus);
+    end = chrono::high_resolution_clock::now ();
+    auto kmpDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
+    cout<<"The KMP took :"<<kmpDuration<<" ms"<<" to excute"<<endl;
+    start = chrono::high_resolution_clock::now ();
+    booyerMooreMatcher.match (testdoc , corpus);
+    end = chrono::high_resolution_clock::now ();
+    auto bmDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
+    cout<<"The Booyer Moore took :"<<bfDuration<<" ms"<<" to excute"<<endl;
+    Document doubledtestdoc;
+    doubledtestdoc.setTitle("doubleTestDoc");
+    doubledtestdoc.setContent(testdoc.getContent()+testdoc.getContent());
+    Corpus doubledCorpus;
+    for(vector<Document>::iterator itr=corpus.getDocuments.begin();itr!=corpus.getDocuments.end();itr++)
+    {
+        Document tempdoc;
+        tempdoc.setTitle(itr->getTitle());
+        tempdoc.setContent(itr->getContent()+itr->getContent());
+        doubledCorpus.addDocument(tempdoc);
     }
-    
+    auto start = chrono::high_resolution_clock::now ();
+    bruteForceMatcher.match (testdoc , corpus);
+    auto end = chrono::high_resolution_clock::now ();
+    auto bfDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
+    cout<<"The brute Force took :"<<bfDuration<<" ms"<<" to excute"<<endl;
+    start = chrono::high_resolution_clock::now ();
+    rabinKarpMatcher.match (testdoc , corpus);
+    end = chrono::high_resolution_clock::now ();
+    auto rkDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
+    cout<<"The rabin karp took :"<<rkDuration<<" ms"<<" to excute"<<endl;
+    start = chrono::high_resolution_clock::now ();
+    kmpMatcher.match (testdoc , corpus);
+    end = chrono::high_resolution_clock::now ();
+    auto kmpDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
+    cout<<"The KMP took :"<<kmpDuration<<" ms"<<" to excute"<<endl;
+    start = chrono::high_resolution_clock::now ();
+    booyerMooreMatcher.match (testdoc , corpus);
+    end = chrono::high_resolution_clock::now ();
+    auto bmDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
+    cout<<"The Booyer Moore took :"<<bfDuration<<" ms"<<" to excute"<<endl;
+
     return 0;
 }
