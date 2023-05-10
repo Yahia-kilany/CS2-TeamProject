09ec45da89fe0ade3fd8b3f7f2d689da8a5ff2ec
yahia <y_kilany@aucegypt.edu>
2023-05-10 15:55:23 +0300

    modified syntax errors

--- Matcher.cpp
+++ Matcher.cpp
@@ -7,18 +7,6 @@ CS2 project: 2- Simple palagarism detection utility using string matching
  * Matcher implementation
  */
 
-virtual size_t Matcher::getMemoryUsage() {
-    return null;
-}
-
-/**
- * @param testDoc
- * @param corpus
- * @return vector<string>
- */
-virtual vector<string> Matcher::match(const Document& testDoc, const Corpus& corpus) {
-    return null;
-}
 
 vector<string> Matcher::splitIntoSentences(const Document& doc) {
 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <Document.h>
-#include <Corpus.h>
-#include <Matcher.h>
+#include "Document.h"
+#include "Corpus.h"
+#include "Matcher.h"
 #include <chrono>
-#include <BruteForceMatcher.h>
-#include <RabinKarpMatcher.h>
-#include <kmpMatcher.h>
-#include <BooyerMooreMatcher.h>
+#include "BruteForceMatcher.h"
+#include "RabinKarpMatcher.h"
+#include "kmpMatcher.h"
+#include "BooyerMooreMatcher.h"
 using namespace std;
 int main () {
     
@@ -87,32 +87,32 @@ int main () {
     doubledtestdoc.setTitle ("doubleTestDoc");
     doubledtestdoc.setContent (testdoc.getContent () + testdoc.getContent ());
     Corpus doubledCorpus;
-    for (vector<Document>::iterator itr = corpus.getDocuments.begin ();itr != corpus.getDocuments.end ();itr++)
+    for (vector<Document>::iterator itr = corpus.getDocuments().begin ();itr != corpus.getDocuments().end ();itr++)
     {
         Document tempdoc;
         tempdoc.setTitle (itr->getTitle ());
         tempdoc.setContent (itr->getContent () + itr->getContent ());
         doubledCorpus.addDocument (tempdoc);
     }
-    auto start = chrono::high_resolution_clock::now ();
+    start = chrono::high_resolution_clock::now ();
     bruteForceMatcher.match (testdoc , corpus);
-    auto end = chrono::high_resolution_clock::now ();
-    auto bfDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
+     end = chrono::high_resolution_clock::now ();
+     bfDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
     cout << "The brute Force took :" << bfDuration << " ms" << " to excute" << endl;
     start = chrono::high_resolution_clock::now ();
     rabinKarpMatcher.match (testdoc , corpus);
     end = chrono::high_resolution_clock::now ();
-    auto rkDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
+     rkDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
     cout << "The rabin karp took :" << rkDuration << " ms" << " to excute" << endl;
     start = chrono::high_resolution_clock::now ();
     kmpMatcher.match (testdoc , corpus);
     end = chrono::high_resolution_clock::now ();
-    auto kmpDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
+     kmpDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
     cout << "The KMP took :" << kmpDuration << " ms" << " to excute" << endl;
     start = chrono::high_resolution_clock::now ();
     booyerMooreMatcher.match (testdoc , corpus);
     end = chrono::high_resolution_clock::now ();
-    auto bmDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
+     bmDuration = chrono::duration_cast<chrono::microseconds>(end - start).count ();
     cout << "The Booyer Moore took :" << bfDuration << " ms" << " to excute" << endl;
 
     cout << "The Brute Force object is taking " << bruteForceMatcher.getMemoryUsage () << " bytes from memory" << endl;
