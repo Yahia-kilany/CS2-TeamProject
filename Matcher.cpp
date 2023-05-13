1f185fb1751f54018017c97bddfe4efeea901893
Yousef <Yousef@DESKTOP-3O45960>
2023-05-13 20:49:52 +0300

    added comments

--- Matcher.cpp
+++ Matcher.cpp
@@ -3,38 +3,39 @@ CS2 project: 2- Simple palagarism detection utility using string matching
  */
 #include "Matcher.h"
 
-/**
- * Matcher implementation
- */
+ /**
+  * Matcher implementation
+  */
 
-
+  //This function splits a document into individual sentences.
+  //eturn A vector of strings, where each string is an individual sentence from the document.
 vector<string> Matcher::splitIntoSentences(const Document& doc) {
 
-        vector<string> sentences;
-        string sentence;
-        char c;
-        for (int i=0;i<doc.getContent().size();i++) {
-            c=doc.getContent().at(i);
-            if (c == '.' || c == '?' || c == '!') {
-                if (!sentence.empty()) {
-                    i++;
-                    sentences.push_back(sentence);
-                    sentence.clear();
-                }
-            }
-            else if (c == '\n') {
-                if (!sentence.empty()) {
-                    i++;
-                    sentences.push_back(sentence);
-                    sentence.clear();
-                }
-            }
-            else {
-                sentence.push_back(c);
+    vector<string> sentences;// initialize an empty vector to store the sentences
+    string sentence; // initialize an empty string to store each sentence
+    char c;
+    for (int i = 0;i < doc.getContent().size();i++) {// iterate through the characters in the document's content
+        c = doc.getContent().at(i);
+        if (c == '.' || c == '?' || c == '!') { // if a sentence-ending punctuation mark is encountered
+            if (!sentence.empty()) {// check if the sentence is non-empty
+                i++; // increment the iterator to skip the punctuation mark
+                sentences.push_back(sentence);// add the sentence to the vector of sentences
+                sentence.clear();// reset the sentence variable
             }
         }
-        if (!sentence.empty()) {
-            sentences.push_back(sentence);
+        else if (c == '\n') { // if a newline character is encountered
+            if (!sentence.empty()) {// check if the sentence is non-empty
+                i++;// increment the iterator to skip the newline character
+                sentences.push_back(sentence); // add the sentence to the vector of sentences
+                sentence.clear();// reset the sentence variable
+            }
         }
-        return sentences;
+        else { // if the character is not a sentence-ending punctuation mark or a newline 
+            sentence.push_back(c);// add the character to the current sentence
+        }
     }
+    if (!sentence.empty()) {// if there is a non-empty sentence left over
+        sentences.push_back(sentence);// add it to the vector of sentences
+    }
+    return sentences;
+}
